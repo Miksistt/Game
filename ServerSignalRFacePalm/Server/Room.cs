9ba5f4580d0db436a43c191e767ed813bf2b1983
Miksist <Miksist000@gmail.com>
2025-11-25 23:04:40 +1000

    (new)

---
+++ ServerSignalRFacePalm/Server/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerSignalRFacePalm.Server
+{
+    public class Room
+    {
+        public string Number { get; set; } = string.Empty;
+        public Dictionary<string, Player> PlayerState { get; set; } = new Dictionary<string, Player>();
+        public string CurrentPlayer { get; set; } = string.Empty;
+    }
+}
