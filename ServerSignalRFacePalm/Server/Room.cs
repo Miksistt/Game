fa99bc9c098e8277e62ec7916772914b11325a2e
Miksist <155412283+Miksistt@users.noreply.github.com>
2025-11-27 01:07:22 +1000

    Delete ServerSignalRFacePalm directory

--- ServerSignalRFacePalm/Server/Room.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServerSignalRFacePalm.Server
-{
-    public class Room
-    {
-        public string Number { get; set; } = string.Empty;
-        public Dictionary<string, Player> PlayerState { get; set; } = new Dictionary<string, Player>();
-        public string CurrentPlayer { get; set; } = string.Empty;
-    }
-}
